763abb395d20682215f7eb65e1781ec910503121
ruhyuh <79058627+yaraaanee@users.noreply.github.com>
2021-09-21 15:30:21 +0800

    changeee

--- resources/views/org/home.blade.php
+++ resources/views/org/home.blade.php
@@ -139,14 +139,6 @@
               </template>
             </li>
           </ul>
-          <div class="px-6 my-6">
-            <button
-              class="flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-            >
-              <!-- Create account -->
-              <span class="ml-2" aria-hidden="true">+</span>
-            </button>
-          </div>
         </div>
       </aside>
       <!-- Mobile sidebar -->
@@ -176,7 +168,7 @@
         <div class="py-4 text-gray-500 dark:text-gray-400">
           <a
             class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
-            href="#"
+            href="/org/home"
           >
             Cardinal Connect
           </a>
@@ -188,7 +180,7 @@
               ></span>
               <a
                 class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                href="index.html"
+                href="/org/home"
               >
                 <svg
                   class="w-5 h-5"
@@ -212,7 +204,7 @@
             <li class="relative px-6 py-3">
               <a
                 class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="forms.html"
+                href="/org/manage"
               >
                 <svg
                   class="w-5 h-5"
@@ -281,12 +273,12 @@
                   <li
                     class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                   >
-                    <a class="w-full" href="org/logs"> Logs </a>
+                    <a class="w-full" href="/org/logs"> Logs </a>
                   </li>
                   <li
                     class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                   >
-                    <a class="w-full" href="pages/forgot-password.html">
+                    <a class="w-full" href="#">
                       Application
                     </a>
                   </li>
@@ -299,14 +291,6 @@
               </template>
             </li>
           </ul>
-          <div class="px-6 my-6">
-            <button
-              class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-            >
-              <!-- Create account -->
-              <span class="ml-2" aria-hidden="true">+</span>
-            </button>
-          </div>
         </div>
       </aside>
       <div class="flex flex-col flex-1 w-full">
